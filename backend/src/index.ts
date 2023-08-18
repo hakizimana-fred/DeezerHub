2a104a3fc5eb14f51fb1707788b1624edb8df396
Hakizimana Fred <hakifred20@gmail.com>
2023-08-18 13:53:18 +0300

    basic server set up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello there");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const start = () => {
+  app.get("/healthcheck", (_req, res) => res.send("smth is running"));
+
+  app.listen(process.env.PORT, () =>
+    console.log(`server is running on port ${process.env.PORT}`)
+  );
+};
+
+void start();
