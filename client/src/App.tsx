0a8d4a4c400bbffe337728d9c89cdbd633a0d3d3
Hakizimana Fred <hakifred20@gmail.com>
2023-08-18 18:37:12 +0300

    search implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,87 @@
-import { Container, Paper, Input, Button, Divider, Title } from "@mantine/core";
-import { QueryClientProvider, queryClient } from "./rtqClient";
-import { ReactQueryDevtools } from "react-query/devtools";
+import {
+  Container,
+  Paper,
+  Text,
+  Input,
+  Button,
+  Divider,
+  Title,
+} from "@mantine/core";
+import React, { useState } from "react";
+import axios from "axios";
+import { queryClient } from "./rtqClient";
+import { useQuery } from "react-query";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+
+  async function getTracks() {
+    if (search.trim() !== "") {
+      const {
+        data: { tracks },
+      } = await axios.get(
+        `http://localhost:5000/api/v1/search?track=${search}`
+      );
+      return tracks;
+    }
+  }
+
+  const {
+    data: tracks,
+    isLoading,
+    isError,
+  } = useQuery("tracks", getTracks, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  if (isError) return <div>Request Failed</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await queryClient.invalidateQueries("tracks");
+  };
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Container size="xs" mt={30}>
-        <Paper shadow="xs">
-          <Title align="center" order={2}>
-            Deezer search Hub
-          </Title>
-          <Divider />
-          <Input placeholder="Enter track name" size="lg" />
-          <Button fullWidth size="lg" style={{ marginTop: "1rem" }}>
+    <Container size="xs" mt={30}>
+      <Paper shadow="xs">
+        <Title align="center" order={2}>
+          Deezer search Hub
+        </Title>
+        <Divider />
+        <form onClick={handleSubmit}>
+          <Input
+            value={search}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearch(e.target.value)
+            }
+            placeholder="Enter track name"
+            size="lg"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            size="lg"
+            style={{ marginTop: "1rem" }}
+          >
             Search
           </Button>
-        </Paper>
-      </Container>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+        </form>
+        <div style={{ marginTop: "1rem" }}>
+          {tracks &&
+            tracks.map((track: any) => (
+              <Paper key={track.artist_id} style={{ marginBottom: "0.5rem" }}>
+                <Text weight={700}>{track.artist_name}</Text>
+                <Text>{track.album_name}</Text>
+                <Text>Duration: {track.duration} seconds</Text>
+              </Paper>
+            ))}
+        </div>
+      </Paper>
+    </Container>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClientProvider, queryClient } from "./rtqClient.ts";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
